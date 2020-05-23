8a8326c16545a9789f50264f338fb83bfd68df1e
Moly <64364914+molyrichez@users.noreply.github.com>
2020-05-23 16:59:49 +0200

    Batch update

--- js/script.js
+++ js/script.js
@@ -3,5 +3,10 @@
 $(function() {
     $('.pane .btn, .pane .btn2').click(function (){
         $('.pane').toggleClass('open')
+        return false;
+    })
+
+    $('.pane').click(function() {
+        $('.pane').removeClass('open')
     })
 })
