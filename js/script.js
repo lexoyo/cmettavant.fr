c8018e2776c3a242049568746518115bbd5acd87
Alex Hoyau <a.hoyau@silexlabs.org>
2020-08-14 17:01:50 +0200

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 
 
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1278},"pages":[{"id":"page-carole-mettavant-presentation","displayName":"Carole Mettavant - presentation","link":{"linkType":"LinkTypePage","href":"#!page-carole-mettavant-presentation"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-en-savoir-plus","displayName":"en savoir plus","link":{"linkType":"LinkTypePage","href":"#!page-en-savoir-plus"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+
 $(function() {
     $('.pane .btn, .pane .btn2').click(function (){
         $('.pane').toggleClass('open')
